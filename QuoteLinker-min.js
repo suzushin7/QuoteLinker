8f99606fb59017545d43fccc71a0be66cd4d7222
Shingo Suzuki <suzushinlab@gmail.com>
2021-08-16 01:58:20 +0900

    Add files via upload

---
+++ QuoteLinker-min.js
@@ -0,0 +1,1 @@
+javascript:(function (t, u, s) {var a = '<a href="'+u+'" title="'+t+'">'+t+'</a>';var code = '<blockquote cite="'+u+'"><p>'+s+'<br />by '+a+'</p></blockquote>';prompt('QuoteLinker', code);})(document.title, location.href, window.getSelection().toString());
